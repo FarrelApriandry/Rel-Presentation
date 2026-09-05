be32ceaae0053ae8d3fad9d90d17b178ccac78f8
re4l1ly <fapriandry@gmail.com>
2026-09-05 17:42:51 +0700

    Remove unused static files: deleted React bundle, favicon.ico, and favicon.svg

--- .vercel/output/_functions/chunks/supabase_DdIAacED.mjs
+++
@@ -1,4 +0,0 @@
-import { createClient } from "@supabase/supabase-js";
-var supabase = createClient("https://wkgfgrujsfdcdwbdjfjl.supabase.co", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndrZ2ZncnVqc2ZkY2R3YmRqZmpsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODg1OTU4NTcsImV4cCI6MjEwNDE3MTg1N30.7tIZcRVTDDbkZWBZKV_3PWZ90sUnQXKTkmRo_NWie00");
-//#endregion
-export { supabase as t };
